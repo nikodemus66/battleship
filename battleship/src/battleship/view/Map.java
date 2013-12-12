18b6697a25c61af9a197b506381b6459c4c0521d
Michael <Michael@User-HP>
2013-12-12 11:33:38 +0100

    -- Map erweitert

--- battleship/src/battleship/view/Map.java
+++ battleship/src/battleship/view/Map.java
@@ -6,47 +6,81 @@
 
 package battleship.view;
 
+import battleship.controller.Engine;
 import battleship.model.*;
+import battleship.model.Point;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.util.Random;
 /**
  *
  * @author Michael Zihlmann
  */
-public class Map extends JPanel{
+public class Map extends JPanel {
     
     private int posx;
     private int posy;   
     public static final int MAP_SIZE = 400;
-    GridLayout layout;
-    MouseListener listener;
-    MouseEvent me;
-    int[] coords;
+    private MouseListener listener;
+    private MouseEvent me;
+    private int[] coords;
+    private JPanel field;
+
 
 
     public Map(){
         
         super(new GridLayout(10,10));
         setPreferredSize(new Dimension(MAP_SIZE, MAP_SIZE));
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 5));
+        for (int i =0; i<(10*10); i++){
+            field = new JPanel();
+            field.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+            field.setBackground(Color.BLUE);
+            field.addMouseListener(listener);
+            this.add(field);
+            
+        }
+        
         
     }
     
+    
     public int[] getCoords(){
         
-        posy = me.getY();
-        posx = me.getX();
         listener.mouseClicked(me);
+        posy = (int) me.getY() * 100;
+        posx = (int) me.getX() * 100;
+        
+        System.out.println(posy);
         
         return coords;
     }
     
-    public void update(Grid g){
-             
+    public int[] getField(){
+                
+        listener.mouseClicked(me);
+        posy = (int) me.getY() * 100;
+        posx = (int) me.getX() * 100;
+        return coords;
+    }
     
+    
+    public void update(){
+        for (int i =0; i<(10*10); i++){
+           
+            field.setBorder(BorderFactory.createLineBorder(Color.BLACK, 2));
+            field.setBackground(Color.BLUE);
+            this.add(field);
+            
+            
+        
+        }
+                    
+              
+                
       
     }
 
@@ -54,8 +88,9 @@ public class Map extends JPanel{
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                JFrame frame = new JFrame("Game");
+                JFrame frame = new JFrame("Map");
                 Map map = new Map();
+                map.update();
                 frame.add(map);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 frame.pack();
@@ -64,3 +99,4 @@ public class Map extends JPanel{
         });
     }
 }
+    
